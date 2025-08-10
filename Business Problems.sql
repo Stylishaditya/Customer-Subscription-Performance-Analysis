f90714587c139d143572d7a39f4cd53c0f23165c
Tulsidas Telange <164472963+Stylishaditya@users.noreply.github.com>
2025-08-10 17:37:58 +0530

    Update Business Problems.sql

--- Business Problems.sql
+++ Business Problems.sql
@@ -1,17 +1,26 @@
 -- Business Problems.
 
+#Customer Insights:
 1.What is the average age of customers by subscription type and location?
 2.Which signup channel brings in the most Premium subscribers?
 3.How many customers have been active for more than 6 months since signup?
 4.What percentage of customers from each location are Premium users?
+
+#Usage & Engagement:  
 5.Which content category has the highest total minutes watched per month?
 6.Do Premium subscribers use the platform for more minutes on average than Basic ones?
 7.Is there a seasonal trend in average daily usage minutes?
+
+#Payments & Revenue:  
 8.What is the total revenue per month by payment method?
 9.Which subscription type contributes the most to revenue?
 10.What is the payment failure rate by payment method?
 11.Are Premium subscribers more likely to have successful payments than Basic ones?
+
+#Support & Retention:  
 12.Which issue type has the longest average resolution time?
 13.How many customers opened more than one support ticket in the last 6 months?
+
+#advance question:  
 14.Which top 3 platform (Netflix, Spotify, Disney+, etc.) generates the most revenue?
 15.How many new customers join each year by platform?
